8b9bf241c51b52b5f91c022678f1608397cb6435
ilovedc10 <ilovedc10@naver.com>
2026-05-14 19:13:14 +0900

    CHange1
    Change

---
+++ Assets/Scripting/MainMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+// using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] private GameObject settingPanel;
+
+    public void OnClickStart()
+    {
+        // Start 버튼 클릭 시 씬 이동
+        // SceneManager.LoadScene("__Scene");
+    }
+
+    public void OnClickSetting()
+    {
+        if (settingPanel != null)
+        {
+            settingPanel.SetActive(true);
+        }
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
